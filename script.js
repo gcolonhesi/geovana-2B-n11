2123ceb465821066d3e12458acfcee60d4e8fd98
gcolonhesi <161721965+gcolonhesi@users.noreply.github.com>
2024-06-18 09:59:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const titulo = document.getElementById('titulo');
+    const corSelect = document.getElementById('cor');
+    corSelect.addEventListener('change', () => {
+    titulo.style.color = corSelect.value;
+    });
+    });
+    
